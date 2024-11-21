c8e44b8e7d1e0098b5dd5c8f5ee922285956924c
joaokmartins <joaomartins@live.it>
2024-11-21 19:50:14 -0400

    Formulário de registro

---
+++ Novembro-24/Semana 4/Dia 21/register-form/src/app/RegisterForm.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState } from 'react';
+
+type RegisterForm = {
+    username:string,
+    useremail:string,
+    userpassword:string,
+}
+
+export default RegisterForm() {
+  
+  const [registerstate, setRegisterState] = useState<RegisterForm> ({
+    username:'',
+    useremail:'',
+    userpassword:'',   
+});
+
+const handleNameChange (e: ChangeEvent(HTMLInputElement)) => {
+  const [name, value] = 
+  setRegisterState(prevState... 
+
+  )
+
+const handleEmailChange (e: ChangeEvent(HTMLInputElement)) => {
+  const [name, value] = 
+  setRegisterState(prevState... 
+      
+  )
+
+const handlePasswordChange (e: ChangeEvent(HTMLInputElement)) => {
+  const [name, value] = 
+  setRegisterState(prevState... 
+        
+  )
+
+const handleSubmit () => {
+  
+} 
+
+
+}
+return (
+
+)
+
+}
