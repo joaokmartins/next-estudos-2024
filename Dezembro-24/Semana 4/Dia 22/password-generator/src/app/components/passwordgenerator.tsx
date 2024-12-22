581646aa5572fe6e4144f242d037056306c4e129
joaokmartins <joaomartins@live.it>
2024-12-22 13:15:27 -0400

    gerador de senhas

---
+++ Dezembro-24/Semana 4/Dia 22/password-generator/src/app/components/passwordgenerator.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {useState} from 'react';
+import styles from '';
+
+interface = {
+    pass:string;
+};
+
+export default function passwordgenerator() {
+  const [item, setItem] = useState<interface> ({''};
+  
+  const generator = {
+    const password = if(item.pass.length > 8 && < 32) {
+      
+    }
+  }
+
+  const handleSubmitButton = alert(``);
+
+  )
+
+  
+
+    return(
+
+      <div className='styles'>
+        <button className='styles'>
+
+        </button>
+
+      </div>
+
+    )
+};
