b3af778ecdfbd7301a68a798352a431f8889043e
joaokmartins <joaomartins@live.it>
2025-01-27 23:01:15 -0400

    Lista com CRUD

---
+++ Janeiro-25/Semana 5/Dia 27/contact-form/src/app/components/ContactForm.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useState } from 'react';
+
+interface List {
+  id: number,
+  username: string,
+  email: string,
+  phone: number,
+};
+
+export default function() {
+  const [myList, setMyList] = useState<List> {
+    id:0,
+    username:'',
+    email:'',
+    phone:'',
+  };
+
+const handleChangeName = (e:React.ChangeEvent<HTMLInputElement> => {
+  {value} = e.target;
+  setMyList((prevState) => {
+    ...prevState,
+  })
+
+const AddNewUser = 
+  if(username.length.trim)  {
+    return()
+  }                                                                                                                                     )
+}
+
+const EditUser = (id: value) {
+  (() =>)
+  setMyList()
+}
+
+const RemoveUser = (id: key)
+  (() =>)
+  setMyList()
+)
+    return(
+      <div className='style'>
+
+    
+      
+      </div>
+
+    )
+}
