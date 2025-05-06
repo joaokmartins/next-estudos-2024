160329380f23ab8bb2505e9c935ea1cf0b0cd972
joaokmartins <joaomartins@live.it>
2025-05-06 07:28:12 -0400

    Exercícios CSS

---
+++ Maio-25/Semana 2/Dia 6/css-exercicios/src/pages/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+import styles from "@/styles/Home.module.css"; // Certifique-se de que o caminho está correto
+
+export default function Home() {
+  return (
+      <div className={styles.transition}>
+      </div>
+  );
+}
