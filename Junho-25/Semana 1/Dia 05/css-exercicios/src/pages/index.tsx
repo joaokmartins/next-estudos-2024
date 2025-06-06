62f73e2aa5bbaafe2a1fb4da2a349291bd877c9e
joaokmartins <joaomartins@live.it>
2025-06-05 21:13:28 -0400

    Exercícios CSS - avançado

---
+++ Junho-25/Semana 1/Dia 05/css-exercicios/src/pages/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import styles from "@/styles/Home.module.css"; // Certifique-se de que o caminho está correto
+
+export default function Home() {
+  return (
+          <div>
+            <div>
+            <div className={styles.card}/>
+            <h2>Card Title</h2>
+            <p>Some example text some example text. John Doe is and archtect and engineer.</p>
+            </div>
+          </div>
+  );
+}
